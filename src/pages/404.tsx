90bfc0e238329e7e9d2b1754cc361fb17579d268
Timmy Willison <4timmywil@gmail.com>
2019-11-15 14:03:15 -0500

    fix(404): fix styles for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
 import Layout from '../components/Layout'
+import React from 'react'
 import SEO from '../components/SEO'
+import { css } from '@emotion/core'
 
 export default function NotFoundPage() {
   return (
     <Layout>
       <SEO title="404: Not Found" />
-      <h1>404</h1>
-      <h3>Page not found :(</h3>
-      <p>The requested page could not be found.</p>
+      <div css={styles.container}>
+        <h1>404</h1>
+        <h3>Page not found :(</h3>
+        <p>The requested page could not be found.</p>
+      </div>
     </Layout>
   )
+}
+
+const styles = {
+  container: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  `
 }

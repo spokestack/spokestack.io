55e801e56ac331921aaabdc6629f9c06788d99b6
Timmy Willison <4timmywil@gmail.com>
2020-01-03 16:33:25 -0500

    feat(account): hook up apollo client and add remote schema

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,19 @@
 // custom typefaces
 import 'prismjs/themes/prism-tomorrow.css'
 
+import { ApolloProvider } from '@apollo/react-hooks'
+import React from 'react'
+import client from './src/apollo/client'
+
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === 'undefined') {
     import('intersection-observer')
     console.log('# IntersectionObserver is polyfilled!')
   }
+}
+
+// eslint-disable-next-line react/prop-types
+export function wrapRootElement({ element }) {
+  return <ApolloProvider client={client}>{element}</ApolloProvider>
 }

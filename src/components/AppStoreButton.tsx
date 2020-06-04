dc048c277dde101c8d10ea54405fa6439d87d883
Timmy Willison <4timmywil@gmail.com>
2020-06-04 15:11:27 -0400

    feat(features): add features page

---
+++ src/components/AppStoreButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import SVGIcon from './SVGIcon'
+import { adjustFontSizeTo } from '../styles/typography'
+import { css } from '@emotion/core'
+
+interface Props {
+  slug: string
+}
+
+export default function AppStoreButton({ slug }: Props) {
+  return (
+    <a href={slug} className="btn" css={styles.button}>
+      <SVGIcon icon="#apple" extraCss={styles.icon} />
+      <div css={styles.text}>
+        <div css={styles.downloadText}>Download on the</div>
+        <div>App Store</div>
+      </div>
+    </a>
+  )
+}
+
+const styles = {
+  button: css`
+    padding-left: 40px;
+    padding-right: 40px;
+  `,
+  icon: css`
+    width: 20px;
+    height: 24px;
+    margin-right: 10px;
+  `,
+  text: css`
+    display: flex;
+    flex-direction: column;
+  `,
+  downloadText: css`
+    font-size: ${adjustFontSizeTo('10px').fontSize};
+    margin-bottom: -5px;
+  `
+}

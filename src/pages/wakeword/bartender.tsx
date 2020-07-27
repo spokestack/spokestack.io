b343097ba21ab17aa44c9ed994f79c91b17d6eb2
Timmy Willison <4timmywil@gmail.com>
2020-07-27 13:47:00 -0400

    fix(wakeword): add a title to the bartender page

--- src/pages/wakeword/bartender.tsx
+++ src/pages/wakeword/bartender.tsx
@@ -2,10 +2,12 @@ import Layout from '../../components/Layout'
 import { PageRendererProps } from 'gatsby'
 import React from 'react'
 import Wakeword from '../../components/Wakeword'
+import SEO from '../../components/SEO'
 
 export default function WakewordBartender({ location }: PageRendererProps) {
   return (
     <Layout location={location}>
+      <SEO title="Spokestack - Wakeword Training for Bartender" />
       <Wakeword assistant="spokestack-bartender" />
     </Layout>
   )

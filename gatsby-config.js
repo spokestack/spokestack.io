a80082366d061289824a5d66f7eb100086475f10
Timmy Willison <4timmywil@gmail.com>
2020-09-19 13:54:39 -0400

    chore(cache): set caching handler for page data to networkfirst

--- gatsby-config.js
+++ gatsby-config.js
@@ -304,7 +304,37 @@ module.exports = {
         icon: 'static/mark.svg'
       }
     },
-    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        workboxConfig: {
+          runtimeCaching: [
+            {
+              // Use cacheFirst since these don't need to be revalidated (same RegExp
+              // and same reason as above)
+              urlPattern: /(\.js$|\.css$|static\/)/,
+              handler: 'CacheFirst'
+            },
+            {
+              // page-data.json files, static query results and app-data.json
+              // are not content hashed
+              urlPattern: /^https?:.*\/page-data\/.*\.json/,
+              handler: 'NetworkFirst'
+            },
+            {
+              // Add runtime caching of various other page resources
+              urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+              handler: 'StaleWhileRevalidate'
+            },
+            {
+              // Google Fonts CSS (doesn't end in .css so we need to specify it)
+              urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+              handler: 'StaleWhileRevalidate'
+            }
+          ]
+        }
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-typography',

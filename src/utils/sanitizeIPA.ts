11f289b1049ce3f5d89e3f2df9788df1d9fb2375
Timmy Willison <4timmywil@gmail.com>
2020-09-22 15:24:53 -0400

    fix(sythesize): ensure ssml is always wrapped even when no IPA

--- src/utils/sanitizeIPA.ts
+++ src/utils/sanitizeIPA.ts
@@ -3,13 +3,10 @@ const rbrackets = /[{}]/g
 
 export default function sanitizeIPA(text: string) {
   if (rIPA.test(text)) {
-    text =
-      '<speak>' +
-      text.replace(rIPA, '<phoneme aplhabet="ipa" ph="$1"></phoneme>') +
-      '</speak>'
+    text = text.replace(rIPA, '<phoneme alphabet="ipa" ph="$1"></phoneme>')
   }
   if (rbrackets.test(text)) {
     return null
   }
-  return text
+  return `<speak>${text}</speak>`
 }

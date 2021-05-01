ff8747632185cbda38e4497f6660c3ee4cfea508
Timmy Willison <4timmywil@gmail.com>
2021-05-01 12:46:08 -0400

    chore(config): update android version

--- gatsby-config.js
+++ gatsby-config.js
@@ -245,7 +245,7 @@ module.exports = {
             resolve: 'gatsby-remark-find-replace',
             options: {
               replacements: {
-                ANDROID_VERSION: '11.2.0',
+                ANDROID_VERSION: '11.3.0',
                 ANDROID_TRAY_VERSION: '0.4.4'
               }
             }

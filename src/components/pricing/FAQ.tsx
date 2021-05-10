d980a01602be6a2807575130c6ba0cb12c17e5cc
Noel Weichbrodt <noel@weichbrodt.org>
2021-05-10 09:11:24 -0400

    fix(pricing): faq content updates

--- src/components/pricing/FAQ.tsx
+++ src/components/pricing/FAQ.tsx
@@ -21,63 +21,65 @@ export default function FAQ({ contact }: Props) {
       </div>
       <div css={styles.questions}>
         <Question
-          question="What's the difference between personal and universal models?"
-          answer=""
+          question="What is the starting cost of Spokestack Enterprise?"
+          answer="Spokestack Enterprise is priced according to your custom requirements, designed in consultation with your company's unique business needs."
         />
         <Question
-          question="How many keywords can a keyword model have?"
-          answer=""
+          question="Does Spokestack Enterprise charge for cloud requests?"
+          answer="Spokestack Entrprise pricing is tailored to your precise use case--unmetered cloud requests included."
         />
         <Question
-          question="Do you do any pre-processing on the audio input to filter out background noise?"
+          question="How can I use Spokestack?"
           answer={
             <p>
-              Many of our platform libraries offer components that perform
-              preprocessing like acoustic noise suppression and gain control.
-              These components have tunable configurations; see our API
-              references (
-              <a href="https://github.com/spokestack/react-native-spokestack#api-documentation">
-                React Native
-              </a>{' '}
-              |{' '}
-              <a href="https://spokestack.github.io/spokestack-ios/index.html">
-                iOS
-              </a>{' '}
-              |{' '}
-              <a href="https://www.javadoc.io/doc/io.spokestack/spokestack-android/latest/index.html">
-                Android
-              </a>{' '}
-              | <a href="/docs/python/getting-started">Python</a>) for more
-              details. In general, Spokestack&apos;s speech processing pipeline
-              is designed to be extensible, so preprocessing stages designed to
-              handle specific environments can be created and added by clients
-              as needed.
+              Spokestack offers low-code integrations for many other voice
+              technology providers, as well as offering our own{' '}
+              <a href="https://spokestack.io/docs/node">Node</a>,{' '}
+              <a href="https://spokestack.io/docs/react-native">React Native</a>
+              , <a href="https://spokestack.io/docs/ios">iOS</a>,{' '}
+              <a href="https://spokestack.io/docs/android">Android</a>, and{' '}
+              <a href="https://spokestack.io/docs/python">Python</a> platform
+              SDKs that make it easy to put voice into your software.
             </p>
           }
         />
         <Question
-          question="How many samples are needed for highest quality personal model training?"
-          answer=""
-        />
-        <Question
           question="What free universal models are available?"
-          answer=""
+          answer="We offer our highest quality TTS male voice of a Hollywood voice actor in a professional recording studio for free. In addition, we offer a free universal 'Spokestack' wake word model, digits and commands keyword models, and a trivia, Minecraft recipe, and high-low NLU models."
         />
-        <Question
-          question="I want to train my own model to use in Spokestack. What inputs and outputs should I expect?"
-          answer=""
-        />
-        <Question question="What is Spokestack ASR's WER?" answer="" />
-        <Question
-          question="What is a personal wake word model’s recall accuracy?"
-          answer=""
-        />
-        <Question question="What is Spokestack’s TTS MOS?" answer="" />
         <Question
           question="What happens when I hit my usage limits?"
-          answer=""
+          answer="There are no penalties associated with reaching your cloud API usage limits. We will contact you to determine the best plan for your cloud API usage needs."
         />
-        <Question question="What are my payment options?" answer="" />
+        <Question
+          question="What are my payment options?"
+          answer="Spokestack accepts payment from all major credit cards and  with Google Pay."
+        />
+        <Question
+          question="Why are custom wake words & keywords so expensive?"
+          answer={
+            <p>
+              Spokestack believes that the future of human-machine interface in
+              software incorporates voice. We intentionally keep the majority of
+              our offerings very affordable in an effort to speed the adoption
+              of voice. In fact, if you compare the cost of creating a custom
+              wake word with Spokestack to that of making one with one of our
+              competitors with similar performance, you&aposll find that our
+              price is still rather low. Making a wake word or keyword spotting
+              model that performs well for a wide variety of people requires
+              collecting a substantial amount of data; it&aposs not something
+              that you can just generate. We take care of that data collection
+              on your behalf, and we&aposll notify you when the task is
+              complete, and your model is ready to train. If you have a
+              substantial user base already, there are ways to bootstrap this
+              data collection, and we&aposre happy to help with that; just
+              <a href="mailto:hello@spokestack.io?subject=Bootstrapping data for custom model">
+                get in touch
+              </a>
+              !
+            </p>
+          }
+        />
       </div>
     </div>
   )

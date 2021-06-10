6c9e18c2766d7d309ca2f7fcd9afb7c22e2dbcfe
Noel <noel@weichbrodt.org>
2021-06-10 15:28:28 -0400

    chore(seo): update homepage seo metadata + tweak copy for maker banner (#197)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,10 +26,7 @@ export default function Index({ data }: Props) {
     <Layout
       banner={
         <Banner href="/account/create">
-          <p>
-            Spokestack Maker - Self-Service Tools for Personal Voice Model
-            Creation
-          </p>
+          <p>Spokestack Maker - Painless Voice Interface Prototyping!</p>
         </Banner>
       }
       extraCss={styles.container}

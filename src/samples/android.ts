e3fce4a74ffbb7058cca089dfa8c97bae4568662
Josh Ziegler <josh@pylon.com>
2021-06-25 15:24:46 -0400

    fix: update Android overview and code samples

--- src/samples/android.ts
+++ src/samples/android.ts
@@ -1,7 +1,17 @@
+export const asr = `val spokestack = Spokestack.Builder()
+    .withoutWakeword()
+    .withoutNlu()
+    .withoutTts()
+    // listener inherits from SpokestackAdapter
+    .addListener(listener)
+    .build()
+spokestack.start()
+`
+
 export const wakeword = `val spokestack = Spokestack.Builder()
-    .setProperty("wake-detect-path", "path/to/detect.tflite")
-    .setProperty("wake-encode-path", "path/to/encode.tflite")
     .setProperty("wake-filter-path", "path/to/filter.tflite")
+    .setProperty("wake-encode-path", "path/to/encode.tflite")
+    .setProperty("wake-detect-path", "path/to/detect.tflite")
     .withoutNlu()
     .withoutTts()
     // listener inherits from SpokestackAdapter
@@ -11,9 +21,10 @@ spokestack.start()
 `
 
 export const keyword = `val spokestack = Spokestack.Builder()
-    .setProperty("keyword-detect-path", "path/to/detect.tflite")
-    .setProperty("keyword-encode-path", "path/to/encode.tflite")
+    .withPipelineProfile(VADTriggerKeywordASR::class.qualifiedName)
     .setProperty("keyword-filter-path", "path/to/filter.tflite")
+    .setProperty("keyword-encode-path", "path/to/encode.tflite")
+    .setProperty("keyword-detect-path", "path/to/detect.tflite")
     .setProperty("keyword-metadata-path", "path/to/metadata.json")
     .withoutNlu()
     .withoutTts()
@@ -23,37 +34,30 @@ export const keyword = `val spokestack = Spokestack.Builder()
 spokestack.start()
 `
 
+export const nlu = `val spokestack = Spokestack.Builder()
+    .withoutSpeechPipeline()
+    .setProperty("nlu-model-path", "path/to/nlu.tflite")
+    .setProperty("nlu-metadata-path", "path/to/metadata.json")
+    .setProperty("wordpiece-vocab-path", "path/to/vocab.txt")
+    .withoutTts()
+    // listener inherits from SpokestackAdapter
+    .addListener(listener)
+    .build()
+val asyncResult = spokestack.classify(utterance)
+`
+
 export const tts = `val spokestack = Spokestack.Builder()
+    .withoutSpeechPipeline()
+    .withoutNlu()
     .setProperty("spokestack-id", "your-client-id")
     .setProperty("spokestack-secret", "your-secret-key")
-    .withoutWakeword()
-    .withoutNlu()
     // applicationContext is available inside all Activitys
     .withAndroidContext(applicationContext)
     // listener inherits from SpokestackAdapter
     .addListener(listener)
     .build()
+
 val request = SynthesisRequest.Builder("hello, world")
     .build()
 spokestack.synthesize(request)
-`
-
-export const nlu = `val nlu = NLUManager.Builder()
-    .setProperty("nlu-model-path", "$cacheDir/nlu.tflite")
-    .setProperty("nlu-metadata-path", "$cacheDir/metadata.json")
-    .setProperty("wordpiece-vocab-path", "$cacheDir/vocab.txt")
-    .addTraceListener(this)
-    .build()
-val asyncResult = nlu?.classify(utterance)
-`
-
-export const asr = `val spokestack = Spokestack.Builder()
-    .withoutWakeword()
-    .withoutNlu()
-    .setProperty("spokestack-id", "your-client-id")
-    .setProperty("spokestack-secret", "your-secret-key")
-    .withAndroidContext(applicationContext)
-    .addListener(listener)
-    .build()
-spokestack.start()
 `
